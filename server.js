443fbab0578d218636f57ccdec02f4ebe09a81bc
gilbrater <gilbrater1@gmail.com>
2017-05-24 18:17:07 +0100

    HEROKU-CHNG

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(__dirname +'/public'));
@@ -10,6 +10,6 @@ app.get('/hello', (req, res)=>{
     res.send("Hello Express");
 });
 
-app.listen(8080, ()=>{
-    console.log("Server is up on port 8080");
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
 });
